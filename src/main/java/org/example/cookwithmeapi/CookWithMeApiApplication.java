bbef231f1006236d520d253e58fdb86219684d88
Marcel <kriollik@gmail.com>
2024-02-03 13:36:01 +0100

    Init

---
+++ src/main/java/org/example/cookwithmeapi/CookWithMeApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.cookwithmeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookWithMeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookWithMeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cookwithmeapi/CookWithMeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cookwithmeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookWithMeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
