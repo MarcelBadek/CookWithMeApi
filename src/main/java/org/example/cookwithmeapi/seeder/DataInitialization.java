038278338e37a377016fadfd6d9d12c5d7626701
Marcel <kriollik@gmail.com>
2024-02-18 14:40:46 +0100

    feat: add random data to data initialization

--- src/main/java/org/example/cookwithmeapi/seeder/DataInitialization.java
+++ src/main/java/org/example/cookwithmeapi/seeder/DataInitialization.java
@@ -1,6 +1,7 @@
 package org.example.cookwithmeapi.seeder;
 
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.example.cookwithmeapi.model.Category;
 import org.example.cookwithmeapi.model.Recipe;
 import org.example.cookwithmeapi.model.account.Administrator;
@@ -25,6 +26,8 @@ public class DataInitialization implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        List<Client> clients = new ArrayList<>();
+
         Client client1 = clientService.create(Client
                 .builder()
                 .username("tBrick")
@@ -51,6 +54,10 @@ public class DataInitialization implements CommandLineRunner {
                 .lastName("Wall")
                 .password("password")
                 .build());
+
+        clients.add(client1);
+        clients.add(client2);
+        clients.add(client3);
 
         Administrator admin1 = administratorService.create(Administrator
                 .builder()
@@ -79,31 +86,51 @@ public class DataInitialization implements CommandLineRunner {
         List<Category> categories = new ArrayList<>();
         categories.add(cat1);
         categories.add(cat2);
+        categories.add(cat3);
 
-        StringBuilder builder1 = new StringBuilder();
-        builder1.append("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit nisl eu pellentesque fringilla. Morbi condimentum erat et dolor maximus bibendum. Mauris pellentesque pharetra posuere. Mauris eu lorem ac nulla facilisis mattis a id arcu. Nulla facilisi. Quisque a justo a ante pharetra tempor. Aliquam gravida semper urna, id sollicitudin purus finibus et. Morbi vitae convallis neque.\n");
-        builder1.append("\n");
-        builder1.append("\"Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse sit amet tellus lacus. Donec ultrices tempor suscipit. Aliquam erat volutpat. Maecenas ultricies ultrices odio in ullamcorper. Donec feugiat ac sapien facilisis venenatis. Cras viverra interdum elit in consectetur. Curabitur non vestibulum turpis, non malesuada lectus. Duis faucibus sem ut nisl efficitur facilisis. Maecenas et placerat libero. Cras eu massa sit amet est iaculis congue. Nam ligula eros, vulputate semper facilisis a, congue tristique lacus. Vivamus semper rutrum ex, ac gravida metus tempor non. Interdum et malesuada fames ac ante ipsum primis in faucibus.\"");
+        for(int i = 0; i < 100; i++) {
+            String name = RandomStringUtils.randomAlphabetic((int)(Math.random() * 20 + 5)).toLowerCase();
+            StringBuilder ingredients = new StringBuilder();
+            for (int j = 0; j < ((int)(Math.random() * 4) + 1); j++) {
+                ingredients.append(RandomStringUtils.randomAlphabetic((int) (Math.random() * 10 + 5)).toLowerCase());
+                ingredients.append(", ");
+            }
+            String description = RandomStringUtils.randomAlphabetic((int)(Math.random() * 500 + 300)).toLowerCase();
 
-        Recipe recipe1 = recipeService.create(Recipe
-                .builder()
-                .name("scrambled eggs")
-                .preparationTime(15)
-                .ingredients("eggs, pepper, salt")
-                .description(builder1.toString())
-                .categories(categories)
-                .build(), client1);
+            Recipe recipe = recipeService.create(Recipe
+                    .builder()
+                    .name(name)
+                    .preparationTime((int)(Math.random() * 120 + 1))
+                    .ingredients(ingredients.toString())
+                    .description(description)
+                    .categories(List.of(categories.get((int)(Math.random() * (categories.size() - 1)))))
+                    .build(), clients.get((int)(Math.random() * (clients.size() - 1))));
+        }
 
-        StringBuilder builder2 = new StringBuilder();
-        builder2.append("Pellentesque aliquam fermentum laoreet. Morbi aliquam lacinia orci non rhoncus. Duis sodales gravida ex ac faucibus. Integer eu dictum tortor. Maecenas justo purus, lobortis non rhoncus ac, lobortis ut nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus id quam quis sem facilisis pellentesque pellentesque eget turpis. Morbi sed velit euismod, ultrices nisl a, sodales metus. Sed varius eget arcu non egestas. Donec ornare cursus leo, sit amet molestie leo elementum lacinia. Praesent eget lacus blandit, mattis ante ac, convallis risus.");
-
-        Recipe recipe2 = recipeService.create(Recipe
-                .builder()
-                .name("sandwiches")
-                .preparationTime(15)
-                .ingredients("bread, butter, ham, cheese")
-                .description(builder2.toString())
-                .categories(categories)
-                .build(), client1);
+//        StringBuilder builder1 = new StringBuilder();
+//        builder1.append("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit nisl eu pellentesque fringilla. Morbi condimentum erat et dolor maximus bibendum. Mauris pellentesque pharetra posuere. Mauris eu lorem ac nulla facilisis mattis a id arcu. Nulla facilisi. Quisque a justo a ante pharetra tempor. Aliquam gravida semper urna, id sollicitudin purus finibus et. Morbi vitae convallis neque.\n");
+//        builder1.append("\n");
+//        builder1.append("\"Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse sit amet tellus lacus. Donec ultrices tempor suscipit. Aliquam erat volutpat. Maecenas ultricies ultrices odio in ullamcorper. Donec feugiat ac sapien facilisis venenatis. Cras viverra interdum elit in consectetur. Curabitur non vestibulum turpis, non malesuada lectus. Duis faucibus sem ut nisl efficitur facilisis. Maecenas et placerat libero. Cras eu massa sit amet est iaculis congue. Nam ligula eros, vulputate semper facilisis a, congue tristique lacus. Vivamus semper rutrum ex, ac gravida metus tempor non. Interdum et malesuada fames ac ante ipsum primis in faucibus.\"");
+//
+//        Recipe recipe1 = recipeService.create(Recipe
+//                .builder()
+//                .name("scrambled eggs")
+//                .preparationTime(15)
+//                .ingredients("eggs, pepper, salt")
+//                .description(builder1.toString())
+//                .categories(categories)
+//                .build(), client1);
+//
+//        StringBuilder builder2 = new StringBuilder();
+//        builder2.append("Pellentesque aliquam fermentum laoreet. Morbi aliquam lacinia orci non rhoncus. Duis sodales gravida ex ac faucibus. Integer eu dictum tortor. Maecenas justo purus, lobortis non rhoncus ac, lobortis ut nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus id quam quis sem facilisis pellentesque pellentesque eget turpis. Morbi sed velit euismod, ultrices nisl a, sodales metus. Sed varius eget arcu non egestas. Donec ornare cursus leo, sit amet molestie leo elementum lacinia. Praesent eget lacus blandit, mattis ante ac, convallis risus.");
+//
+//        Recipe recipe2 = recipeService.create(Recipe
+//                .builder()
+//                .name("sandwiches")
+//                .preparationTime(15)
+//                .ingredients("bread, butter, ham, cheese")
+//                .description(builder2.toString())
+//                .categories(categories)
+//                .build(), client1);
     }
 }
